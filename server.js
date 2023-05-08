d010c50bd7067ce1a7530ff6d9d7fcea11d38a1d
Plant900 <rythodir@gmail.com>
2023-05-07 19:07:18 -0600

    fix typo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http')
 const app = require('./app')
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
