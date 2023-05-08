bafb832fb74d0bac317a2d3072175fdd7c387f00
Plant900 <rythodir@gmail.com>
2023-05-07 18:35:48 -0600

    Add server.js

--- app.js
+++ app.js
@@ -7,7 +7,4 @@ const connectDB = require('./config/mongoDB.js')()
 app.use(express.json())
 app.use('/api', router)
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
